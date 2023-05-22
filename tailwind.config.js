cc3610518555ecd2fa509bddfa2b99e693cfdc54
DESKTOP-6OQIVOM\Nyan Nyan <nyanwinmyo@gmail.com>
2023-05-22 11:10:51 +0630

    hello

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "color-secondary":"#ff7d3b",
+        "color-secondary-white":"white",
+      }
+    },
+  },
+  plugins: [],
+}
+
